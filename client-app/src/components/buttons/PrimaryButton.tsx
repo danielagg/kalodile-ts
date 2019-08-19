fc3992454d3eb2502b893058ccc640e92fcc5727
Daniel Agg <daniel.agg@outlook.com>
2019-08-19 17:16:38 +0200

    Added post ad button to landing banner

--- client-app/src/components/buttons/PrimaryButton.tsx
+++ client-app/src/components/buttons/PrimaryButton.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 
+export enum PrimaryButtonStyle {
+  White,
+  Red
+}
+
 type PrimaryButtonProps = {
   caption: string;
+  customCss?: string;
+  style: PrimaryButtonStyle;
 };
 
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
-  caption
+  caption,
+  customCss,
+  style
 }: PrimaryButtonProps) => {
+  const getBackgroundStyle = () => {
+    const defaultStyle = "bg-gray-100 hover:text-gray-700";
+
+    switch (style) {
+      case PrimaryButtonStyle.White:
+        return defaultStyle;
+      case PrimaryButtonStyle.Red:
+        return "red-gradient hover:text-white text-black";
+      default:
+        return defaultStyle;
+    }
+  };
+
   return (
     <div
-      className="primary-button
-        inline-block
-        px-6 lg:px-12 py-2 lg:py-4
-        font-medium lg:text-lg text-black
-        bg-gray-100
-        cursor-pointer shadow-md
-        hover:text-gray-700"
+      className={`${customCss} ${getBackgroundStyle()} primary-button
+    rounded-full
+    px-6 lg:px-12 py-2 lg:py-4
+    font-medium lg:text-lg text-black
+    cursor-pointer shadow-md
+    `}
     >
       <p>{caption}</p>
     </div>
--- client-app/src/components/landing/CallToActionBanner.tsx
+++ client-app/src/components/landing/CallToActionBanner.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 // Components
-import PrimaryButton from "../buttons/PrimaryButton";
+import PrimaryButton, { PrimaryButtonStyle } from "../buttons/PrimaryButton";
 
 const CallToActionBanner: React.FC = () => {
   return (
     <>
       <div className="landing-container p-6 text-center">
-        <div className="text-white py-2 lg:py-16">
+        <div className="text-white py-2 lg:py-24">
           <h1 className="mt-4 text-2xl lg:text-6xl font-medium">
             Got a gig but no gear?
           </h1>
@@ -18,8 +18,17 @@ const CallToActionBanner: React.FC = () => {
             With Kalodile, you can rent any equipment, or offer your own. You
             specify when and for how much.
           </p>
-          <div className="mt-4 lg:mt-8">
-            <PrimaryButton caption={"View ads"} />
+          <div className="mt-4 lg:mt-8 lg:flex lg:justify-center">
+            <PrimaryButton
+              caption={"View ads"}
+              customCss={"lg:mr-2"}
+              style={PrimaryButtonStyle.White}
+            />
+            <PrimaryButton
+              caption={"Post ad"}
+              customCss={"lg:ml-2 mt-4 lg:mt-0"}
+              style={PrimaryButtonStyle.Red}
+            />
           </div>
         </div>
       </div>
