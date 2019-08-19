7ea1ae3d1169c79053ed9b9426ba0cae91c46f03
Daniel Agg <daniel.agg@outlook.com>
2019-08-19 12:37:58 +0200

    Styling button hover state

--- client-app/src/components/buttons/PrimaryButton.tsx
+++ client-app/src/components/buttons/PrimaryButton.tsx
@@ -8,7 +8,15 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   caption
 }: PrimaryButtonProps) => {
   return (
-    <div className="inline-block px-6 lg:px-12 py-2 lg:py-4 font-medium lg:text-lg text-black bg-gray-100 cursor-pointer hover:text-blue-800">
+    <div
+      className="primary-button
+        inline-block
+        px-6 lg:px-12 py-2 lg:py-4
+        font-medium lg:text-lg text-black
+        bg-gray-100
+        cursor-pointer shadow-md
+        hover:text-gray-700"
+    >
       <p>{caption}</p>
     </div>
   );
